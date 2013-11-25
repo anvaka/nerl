b42e12620658625f40719496d3ef202a99085d6e
anvaka <anvaka@gmail.com>
2013-11-25 12:35:34 -0800

    Want to keep this abstract yet

--- prototype/00 - clock javascript/in/lib/clock.js
+++ prototype/00 - clock javascript/in/lib/clock.js
@@ -1,10 +1,13 @@
 module.exports = Clock;
 
 // explicitly associate Clock with clock.html template:
-require('./nerl').bind(Clock, './clock.html');
+require('nerl').bind(Clock, './clock.html');
 
 function Clock() {
-  var $ = this.initDom();
+  var component = this._initComponent();
+
+  // DOM proxy maps ids from template to named properties
+  var $ = component.getDOMProxy();
 
   setInterval(function () {
     var now = new Date();
--- prototype/02 - expander/in/lib/expander.js
+++ prototype/02 - expander/in/lib/expander.js
@@ -3,8 +3,13 @@ module.exports = Expander;
 require('nerl').bind(Expander, './expander.html');
 
 function Expander() {
-  var $ = this.initDom();
-  var mainContent = $.getContent();
-  var headerContent = $.getContent('header');
+  var component = this._initComponent();
+  // default, no arguments = get default content
+  var mainContent = component.getContent();
+  var headerContent = component.getContent('header');
 
+  mainContent.on('click', function () {
+    var isShown = headerContent.style.display !== 'none';
+    headerContent.style.display = isShown ? 'none' : 'block';
+  });
 }
