bbece664132377c03d86b4253feb01d5be038b6b
anvaka <anvaka@gmail.com>
2013-12-06 23:43:30 -0800

    Taking a new route with angular

--- prototype/00 - clock javascript/in/main.js
+++
@@ -1,8 +0,0 @@
-// this file should be an entry point to browserify command.
-
-// every time when user clicks we want to create a new 'clock' component:
-window.onclick = function () {
-  var Clock = require('./lib/clock');
-  var clock = new Clock();
-  clock.appendTo(document.body);
-}
