2affe37a0fd30073a34eef879d443738653db443
anvaka <anvaka@gmail.com>
2013-12-07 01:53:06 -0800

    28 lines of magic
    These lines should help sharing angular directives on NPM

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 module.exports = nerl;
 
+function nerl() { }
 
-function nerl(entryPoint, resultCallback) {
+var declaredDirectives = {},
+    compileProvider;
 
+nerl.directive = function (preferredName, directiveFactory) {
+  declaredDirectives[preferredName] = directiveFactory;
+  registerDirective(preferredName);
+
+  return directiveFactory;
+};
+
+nerl.bindDirectives = function (module) {
+  module.config(function($compileProvider) {
+    compileProvider = $compileProvider;
+    Object.keys(declaredDirectives).forEach(function (directiveName) {
+      registerDirective(directiveName);
+    });
+  });
+};
+
+function registerDirective(directiveName) {
+  if (compileProvider) {
+    compileProvider.directive(directiveName, declaredDirectives[directiveName]);
+  }
 }
