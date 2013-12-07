92b1b8d9ed73224e96aa6cc797349a61e964401f
anvaka <anvaka@gmail.com>
2013-12-07 01:54:27 -0800

    Added example: bootstrap's alert directive

---
+++ prototype/ag.alert/alert.js
@@ -0,0 +1,15 @@
+module.exports = require('nerl').directive('alert', function () {
+  return {
+    restrict:'EA',
+    templateUrl:'alert.html',
+    transclude:true,
+    replace:true,
+    scope: {
+      type: '=',
+      close: '&'
+    },
+    link: function(scope, iElement, iAttrs) {
+      scope.closeable = "close" in iAttrs;
+    }
+  };
+});
